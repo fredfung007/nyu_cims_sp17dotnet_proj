021925d66a5a2fedf1b0ea4fe141245d81e3aea6
Fred Fung <fred@rubyalliance.com>
2017-04-11 14:01:48 -0400

    Move ICustomer Interface to to Customer Folder

--- ProjectPurple/BusinessLogic/Customer/ICustomer.cs
+++ ProjectPurple/BusinessLogic/Customer/ICustomer.cs
@@ -5,18 +5,18 @@ using System;
 
 namespace BusinessLogic.Customer
 {
-    // Interface for customer/guest who will stay at the hotel.
+    // Interface for customer/guest who will stay at the hotel. 
     interface ICustomer
     {
-        // Return the first name of the customer.
+        // Return the first name of the customer. 
         String getFirstName();
-        // Return the last name of the customer.
+        // Return the last name of the customer. 
         String getLastName();
-        // Return the address of the customer.
+        // Return the address of the customer. 
         IAddress getAddress();
-        // Return the email address of the customer.
+        // Return the email address of the customer. 
         IEmail getEmail();
-        // Return the phone number of the customer.
+        // Return the phone number of the customer. 
         IPhoneNumber getPhoneNumber();
     }
 }
