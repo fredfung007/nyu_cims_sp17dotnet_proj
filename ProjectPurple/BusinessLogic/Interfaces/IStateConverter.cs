738440927552c22ba5630ace81d0f9906dd21853
Fred Fung <fred@rubyalliance.com>
2017-04-11 11:54:25 -0400

    Add StateConverter Interface and its implementation
    Interface for converters from string to state enum 
    Name Converter for U.S. States.

---
+++ ProjectPurple/BusinessLogic/Interfaces/IStateConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLogic.Address
+{
+    // Interface for converters from string to state enum
+    interface IStateConverter
+    {
+        // Convert states to its full names.
+        string GetState(State state);
+        // Convert string to the state enum.
+        State GetStateByName(string name);
+    }
+}
