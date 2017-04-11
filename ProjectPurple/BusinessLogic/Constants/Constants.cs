f150ebbdee6c70dd13f2b04386efaba19ad575fb
Mengdi <wangmengdi@gmail.com>
2017-04-11 13:48:55 -0400

    add constant, add IRoomh

---
+++ ProjectPurple/BusinessLogic/Constants/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Constants
+{ 
+    public enum roomType
+    {
+        DOUBLE_BED_ROOM,
+        QUEEN_ROOM,
+        KING_BED_ROOM,
+        SUITE
+    };
+    class Constants
+    {
+
+    }
+}
---
+++ ProjectPurple/BusinessLogic/Interfaces/IRoom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Constants;
+
+namespace BusinessLogic.Interfaces
+{
+    interface IRoom
+    {
+        List<double> getRoomPriceList(roomType type, DateTime start, DateTime end);
+        double getRoomPrice(roomType type, DateTime date);
+        int getCurrentRoomAvailability(roomType type, DateTime date);
+        bool setRoomInventory(roomType type, int amount);
+        string getRoomDescription(roomType type);
+        bool updateRoomDescription(roomType type, string description);
+        string getRoomAmeneties(roomType type);
+        List<string> getRoomPictureUrls(roomType type);
+        bool updateRoomPictureUrls(roomType type, List<string> urls);
+        bool insertPictureUrl(roomType type, string url);
+    }
+}
