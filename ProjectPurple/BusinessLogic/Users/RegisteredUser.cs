bbd541b1bcc693e1b7a2003289215030fc80d2b1
Fred Fung <fred@rubyalliance.com>
2017-04-11 13:41:05 -0400

    Add Anonymous and Registered Users Class.

---
+++ ProjectPurple/BusinessLogic/Users/RegisteredUser.cs
@@ -0,0 +1,16 @@
+﻿namespace BusinessLogic.Users
+{
+    class RegisteredUser : IUser
+    {
+        private string userName;
+
+        RegisteredUser(string userName)
+        {
+            this.userName = userName;
+        }
+        public string getUserName()
+        {
+            return this.userName;
+        }
+    }
+}
