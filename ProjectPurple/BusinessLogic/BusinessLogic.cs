894e6df6bd6d30b4e95d2ed76e31d85f804b372d
Boyuan Yuan <ylns1314@gmail.com>
2017-04-11 10:37:57 -0400

    BusinessLogic proj created

---
+++ ProjectPurple/BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class BusinessLogic
+    {
+        public BusinessLogic()
+        {
+        }
+    }
+}
