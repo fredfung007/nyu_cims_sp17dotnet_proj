ad33834fb35feef5aa55669c0b54efbc8f025c0c
Boyuan Yuan <ylns1314@gmail.com>
2017-04-11 13:55:23 -0400

    BillingInfo interface added

---
+++ ProjectPurple/BusinessLogic/Interfaces/IBillingInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Address;
+using BusinessLogic.PhoneNumber;
+using BusinessLogic.Email;
+
+namespace BusinessLogic.BillingInfo
+{
+    interface IBillingInfo
+    {
+        IAddress address { get; }
+        IPhoneNumber phoneNumber { get; }
+        IEmail email { get; }
+        string firstName { get; }
+        string lastName { get; }
+    }
+}
