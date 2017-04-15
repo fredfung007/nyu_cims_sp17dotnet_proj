cb3da6f3d5259effd290b4e9c74910fb278ef569
Fred Fung <fred@rubyalliance.com>
2017-04-15 15:57:42 -0400

    Add methods for AuthHandler.

--- ProjectPurple/BusinessLogic/Handlers/AuthHandler.cs
+++ ProjectPurple/BusinessLogic/Handlers/AuthHandler.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DataAccessLayer.Repositories;
 using DataAccessLayer;
 
@@ -14,6 +10,28 @@ namespace BusinessLogic.Handlers
         public AuthHandler()
         {
             authRepository = new AuthRepository(new HotelDataModelContainer());
+        }
+
+        /// <summary>
+        /// Verify the input password for given username versus the password in DB.
+        /// </summary>
+        /// <param name="username">username of a user</param>
+        /// <param name="inputPassword">input password</param>
+        /// <returns></returns>
+        public bool loginUser(string username, string inputPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Verify the input password for given username versus the password in DB.
+        /// </summary>
+        /// <param name="userName">username of a staff</param>
+        /// <param name="inputPassword">input password</param>
+        /// <returns></returns>
+        public bool loginStaff(string userName, string inputPassword)
+        {
+            throw new NotImplementedException();
         }
     }
 }
