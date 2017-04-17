978991463b5651e8f4d07076b7833adcda27ed8e
Boyuan Yuan <by653@nyu.edu>
2017-04-17 14:08:05 -0400

    Add IUserRepository but not implemented yet

---
+++ ProjectPurple/DataAccessLayer/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccessLayer.Constants;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IUserRepository : IDisposable
+    {
+        User GetUser(string username);
+        void InsertUser(User u);
+        void DeleteUser(string username);
+        void UpdateUser(User u);
+        void save();
+    }
+}
