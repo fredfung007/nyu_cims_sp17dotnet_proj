72f7f8d5920b114da7aa82069b754fcb62357691
Boyuan Yuan <by653@nyu.edu>
2017-04-17 16:16:08 -0400

    ReservationHandler work in progress

--- ProjectPurple/BusinessLogic/Handlers/ReservationHandler.cs
+++ ProjectPurple/BusinessLogic/Handlers/ReservationHandler.cs
@@ -16,28 +16,58 @@ namespace BusinessLogic.Handlers
         {
             reservationRepository = new ReservationRepository(new HotelDataModelContainer());
         }
-        Guid MakeReservation(int userId, ROOM_TYPE type, DateTime start, DateTime end)
+
+        /// <summary>
+        /// Create a new Reservation.
+        /// !!!!!!!! Not set User yet !!!!!!!
+        /// </summary>
+        /// <param name="type">ROOM_TYPE</param>
+        /// <param name="start">check-in date</param>
+        /// <param name="end">check-out date</param>
+        /// <param name="guests">list of guests attending</param>
+        /// <returns></returns>
+        Guid MakeReservation(ROOM_TYPE type, DateTime start, DateTime end, List<Guest> guests)
         {
             Reservation r = new Reservation();
-            return Guid.Empty;
+            r.Id = Guid.NewGuid();
+            r.startDate = start;
+            r.endDate = end;
+            r.Guests = guests;
+            r.isPaid = false;
+            r.DailyPrices = new List<DailyPrice>();
+            List<int> prices = (new RoomHandler()).GetRoomPriceList(type, start, end);
+            foreach (int price in prices)
+            {
+                DailyPrice dp = new DailyPrice();
+                dp.Id = r.Id;
+                dp.Date = start;
+                dp.BillingPrice = price;
+                start.AddDays(1);
+                r.DailyPrices.Add(dp);
+            }
+
+            reservationRepository.InsertReservation(r);
+            return r.Id;
         }
+
+        void PayReservation(Guid confirmationNumber, Profile billingInfo)
+        {
+            Reservation r = reservationRepository.getReservation(confirmationNumber);
+            if (r != null)
+            {
+                r.BillingInfo = billingInfo;
+                r.isPaid = true;
+            }
+        }
+
         /// <summary>
         /// Cacnel a reservation by its creator's username and confirmation number
         /// </summary>
-        /// <param name="username">username of the User who created the reservation</param>
         /// <param name="confirmationNumber">confirmation number of the reservation</param>
         /// <returns>true if successfully cancelled</returns>
-        bool CancelReservation(string username, Guid confirmationNumber)
+        void CancelReservation(Guid confirmationNumber)
         {
-            bool isValid = false;
-            Reservation r = reservationRepository.getReservation(confirmationNumber);
-            if (r.User.Username == username)
-            {
-                reservationRepository.DeleteReservation(confirmationNumber);
-                isValid = true;
-            }
-            else { }
-            return isValid;
+            reservationRepository.DeleteReservation(confirmationNumber);
         }
 
         // obsolete
@@ -46,14 +76,15 @@ namespace BusinessLogic.Handlers
         //    return null;
         //}
 
-        List<Reservation> GetUpComingReservations(Guest customer)
+        List<Reservation> GetUpComingReservations(User u)
         {
-            return null;
+            return reservationRepository.getReservationsByUserId(u.Username);
         }
 
-        bool FillGuestInfo(Reservation reservation, List<Guest> customers)
-        {
-            return false;
-        }
+        // obsolete
+        //bool FillGuestInfo(Reservation reservation, List<Guest> customers)
+        //{
+        //    return false;
+        //}
     }
 }
