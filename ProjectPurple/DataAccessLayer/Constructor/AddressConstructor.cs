238619a904c0236c2cc515cec4deac0fa982297b
Fred Fung <fred@rubyalliance.com>
2017-04-18 15:12:57 -0400

    Add constructor Classes, minor bug fixes on EF.

---
+++ ProjectPurple/DataAccessLayer/Constructor/AddressConstructor.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Constants;
+
+namespace DataAccessLayer.Constructor
+{
+    class AddressConstructor
+    {
+        private Address address;
+
+        AddressConstructor()
+        {
+            address = new Address();
+        }
+
+        AddressConstructor addFirstLine(string firstLine)
+        {
+            address.FirstLine = firstLine;
+            return this;
+        }
+
+        AddressConstructor addSecondLine(string secondLine)
+        {
+            address.SecondLine = secondLine;
+            return this;
+        }
+
+        AddressConstructor addCity(string city)
+        {
+            address.City = city;
+            return this;
+        }
+
+        AddressConstructor addState(US_STATE state)
+        {
+            address.State = state;
+            return this;
+        }
+
+        AddressConstructor addZipcode(string zipcode)
+        {
+            address.ZipCode = zipcode;
+            return this;
+        }
+
+        Address build()
+        {
+            return address;
+        }
+    }
+}
