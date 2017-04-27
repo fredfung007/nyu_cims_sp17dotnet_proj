4b5db52a8f303c2081abcfe0b9dc37c514c2e17a
Fred Fung <fred@rubyalliance.com>
2017-04-27 15:36:04 -0400

    Add migration script

--- ProjectPurple/DataAccessLayer/Migrations/201704271928152_Make-AspIdentity-Singular.cs
+++ ProjectPurple/DataAccessLayer/Migrations/201704271928152_Make-AspIdentity-Singular.cs
@@ -1,110 +1,110 @@
+using System.Data.Entity.Migrations;
+
 namespace DataAccessLayer.Migrations
 {
-    using System.Data.Entity.Migrations;
-    
     public partial class MakeAspIdentitySingular : DbMigration
     {
         public override void Up()
         {
             CreateTable(
-                "dbo.Addresses",
-                c => new
+                    "dbo.Addresses",
+                    c => new
                     {
-                        Id = c.Guid(nullable: false),
-                        FirstLine = c.String(nullable: false),
+                        Id = c.Guid(false),
+                        FirstLine = c.String(false),
                         SecondLine = c.String(),
-                        State = c.Int(nullable: false),
-                        ZipCode = c.String(nullable: false),
-                        City = c.String(nullable: false),
+                        State = c.Int(false),
+                        ZipCode = c.String(false),
+                        City = c.String(false)
                     })
                 .PrimaryKey(t => t.Id);
-            
+
             CreateTable(
-                "dbo.AspNetRoles",
-                c => new
+                    "dbo.AspNetRoles",
+                    c => new
                     {
-                        Id = c.String(nullable: false, maxLength: 128),
-                        Name = c.String(nullable: false, maxLength: 256),
+                        Id = c.String(false, 128),
+                        Name = c.String(false, 256)
                     })
                 .PrimaryKey(t => t.Id);
-            
+
             CreateTable(
-                "dbo.AspNetUsers",
-                c => new
+                    "dbo.AspNetUsers",
+                    c => new
                     {
-                        Id = c.String(nullable: false, maxLength: 128),
+                        Id = c.String(false, 128),
                         Email = c.String(maxLength: 256),
-                        EmailConfirmed = c.Boolean(nullable: false),
+                        EmailConfirmed = c.Boolean(false),
                         PasswordHash = c.String(),
                         SecurityStamp = c.String(),
                         PhoneNumber = c.String(),
-                        PhoneNumberConfirmed = c.Boolean(nullable: false),
-                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        PhoneNumberConfirmed = c.Boolean(false),
+                        TwoFactorEnabled = c.Boolean(false),
                         LockoutEndDateUtc = c.DateTime(),
-                        LockoutEnabled = c.Boolean(nullable: false),
-                        AccessFailedCount = c.Int(nullable: false),
-                        UserName = c.String(nullable: false, maxLength: 256),
+                        LockoutEnabled = c.Boolean(false),
+                        AccessFailedCount = c.Int(false),
+                        UserName = c.String(false, 256)
                     })
                 .PrimaryKey(t => t.Id);
-            
+
             CreateTable(
-                "dbo.AspNetUserClaims",
-                c => new
+                    "dbo.AspNetUserClaims",
+                    c => new
                     {
-                        Id = c.Int(nullable: false, identity: true),
-                        UserId = c.String(nullable: false, maxLength: 128),
+                        Id = c.Int(false, true),
+                        UserId = c.String(false, 128),
                         ClaimType = c.String(),
-                        ClaimValue = c.String(),
+                        ClaimValue = c.String()
                     })
                 .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, true)
                 .Index(t => t.UserId);
-            
+
             CreateTable(
-                "dbo.AspNetUserLogins",
-                c => new
+                    "dbo.AspNetUserLogins",
+                    c => new
                     {
-                        LoginProvider = c.String(nullable: false, maxLength: 128),
-                        ProviderKey = c.String(nullable: false, maxLength: 128),
-                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(false, 128),
+                        ProviderKey = c.String(false, 128),
+                        UserId = c.String(false, 128)
                     })
-                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
-                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .PrimaryKey(t => new {t.LoginProvider, t.ProviderKey, t.UserId})
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, true)
                 .Index(t => t.UserId);
-            
+
             CreateTable(
-                "dbo.Profiles",
-                c => new
+                    "dbo.Profiles",
+                    c => new
                     {
-                        Id = c.Guid(nullable: false),
-                        FirstName = c.String(nullable: false),
-                        LastName = c.String(nullable: false),
+                        Id = c.Guid(false),
+                        FirstName = c.String(false),
+                        LastName = c.String(false),
                         Email = c.String(),
                         PhoneNumber = c.String(),
-                        Address_Id = c.Int(nullable: false),
+                        Address_Id = c.Int(false),
                         IdAspNetUsers_Id = c.String(maxLength: 128),
-                        Address_Id1 = c.Guid(),
+                        Address_Id1 = c.Guid()
                     })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Addresses", t => t.Address_Id1)
                 .ForeignKey("dbo.AspNetUsers", t => t.IdAspNetUsers_Id)
                 .Index(t => t.IdAspNetUsers_Id)
                 .Index(t => t.Address_Id1);
-            
+
             CreateTable(
-                "dbo.Reservations",
-                c => new
+                    "dbo.Reservations",
+                    c => new
                     {
-                        Id = c.Guid(nullable: false),
-                        startDate = c.DateTime(nullable: false),
-                        endDate = c.DateTime(nullable: false),
-                        isPaid = c.Boolean(nullable: false),
+                        Id = c.Guid(false),
+                        startDate = c.DateTime(false),
+                        endDate = c.DateTime(false),
+                        isPaid = c.Boolean(false),
                         checkInDate = c.DateTime(),
                         checkOutDate = c.DateTime(),
-                        BillingInfo = c.Guid(nullable: false),
-                        RoomType_Id = c.Guid(nullable: false),
+                        BillingInfo = c.Guid(false),
+                        RoomType_Id = c.Guid(false),
                         User_Username = c.String(maxLength: 30),
-                        AspNetUsers_Id = c.String(maxLength: 128),
+                        AspNetUsers_Id = c.String(maxLength: 128)
                     })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.RoomTypes", t => t.RoomType_Id)
@@ -115,100 +115,99 @@ namespace DataAccessLayer.Migrations
                 .Index(t => t.RoomType_Id)
                 .Index(t => t.User_Username)
                 .Index(t => t.AspNetUsers_Id);
-            
+
             CreateTable(
-                "dbo.DailyPrices",
-                c => new
+                    "dbo.DailyPrices",
+                    c => new
                     {
-                        Id = c.Guid(nullable: false),
-                        BillingPrice = c.Int(nullable: false),
-                        Date = c.DateTime(nullable: false),
-                        Reservation_Id = c.Guid(nullable: false),
+                        Id = c.Guid(false),
+                        BillingPrice = c.Int(false),
+                        Date = c.DateTime(false),
+                        Reservation_Id = c.Guid(false)
                     })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Reservations", t => t.Reservation_Id)
                 .Index(t => t.Reservation_Id);
-            
+
             CreateTable(
-                "dbo.Guests",
-                c => new
+                    "dbo.Guests",
+                    c => new
                     {
-                        Id = c.Guid(nullable: false),
-                        FirstName = c.String(nullable: false),
-                        LastName = c.String(nullable: false),
-                        Reservation_Id = c.Guid(nullable: false),
+                        Id = c.Guid(false),
+                        FirstName = c.String(false),
+                        LastName = c.String(false),
+                        Reservation_Id = c.Guid(false)
                     })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Reservations", t => t.Reservation_Id)
                 .Index(t => t.Reservation_Id);
-            
+
             CreateTable(
-                "dbo.RoomTypes",
-                c => new
+                    "dbo.RoomTypes",
+                    c => new
                     {
-                        Id = c.Guid(nullable: false),
-                        BaseRate = c.Int(nullable: false),
-                        Inventory = c.Int(nullable: false),
-                        Type = c.Int(nullable: false),
-                        Ameneties = c.String(nullable: false),
-                        Description = c.String(nullable: false),
-                        ImageUrl = c.String(),
+                        Id = c.Guid(false),
+                        BaseRate = c.Int(false),
+                        Inventory = c.Int(false),
+                        Type = c.Int(false),
+                        Ameneties = c.String(false),
+                        Description = c.String(false),
+                        ImageUrl = c.String()
                     })
                 .PrimaryKey(t => t.Id);
-            
+
             CreateTable(
-                "dbo.RoomOccupancies",
-                c => new
+                    "dbo.RoomOccupancies",
+                    c => new
                     {
-                        Id = c.Guid(nullable: false),
-                        Date = c.DateTime(nullable: false),
-                        Occupancy = c.Int(nullable: false),
-                        RoomType_Id = c.Guid(nullable: false),
+                        Id = c.Guid(false),
+                        Date = c.DateTime(false),
+                        Occupancy = c.Int(false),
+                        RoomType_Id = c.Guid(false)
                     })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.RoomTypes", t => t.RoomType_Id)
                 .Index(t => t.RoomType_Id);
-            
+
             CreateTable(
-                "dbo.Users",
-                c => new
+                    "dbo.Users",
+                    c => new
                     {
-                        Username = c.String(nullable: false, maxLength: 30),
-                        HashedPassword = c.String(nullable: false),
-                        isRegistered = c.Boolean(nullable: false),
+                        Username = c.String(false, 30),
+                        HashedPassword = c.String(false),
+                        isRegistered = c.Boolean(false),
                         LoyalProgramNumber = c.String(),
                         LoyaltyProgress = c.Int(),
                         LoyaltyYear = c.DateTime(),
-                        Profile_Id = c.Guid(),
+                        Profile_Id = c.Guid()
                     })
                 .PrimaryKey(t => t.Username)
                 .ForeignKey("dbo.Profiles", t => t.Profile_Id)
                 .Index(t => t.Profile_Id);
-            
+
             CreateTable(
-                "dbo.Staffs",
-                c => new
+                    "dbo.Staffs",
+                    c => new
                     {
-                        Username = c.String(nullable: false, maxLength: 30),
-                        HashedPassword = c.String(nullable: false),
+                        Username = c.String(false, 30),
+                        HashedPassword = c.String(false)
                     })
                 .PrimaryKey(t => t.Username);
-            
+
             CreateTable(
-                "dbo.AspNetUserRoles",
-                c => new
+                    "dbo.AspNetUserRoles",
+                    c => new
                     {
-                        RoleId = c.String(nullable: false, maxLength: 128),
-                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(false, 128),
+                        UserId = c.String(false, 128)
                     })
-                .PrimaryKey(t => new { t.RoleId, t.UserId })
-                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
-                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .PrimaryKey(t => new {t.RoleId, t.UserId})
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, true)
                 .Index(t => t.RoleId)
                 .Index(t => t.UserId);
-            
         }
-        
+
         public override void Down()
         {
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
@@ -225,20 +224,20 @@ namespace DataAccessLayer.Migrations
             DropForeignKey("dbo.Profiles", "Address_Id1", "dbo.Addresses");
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
-            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
-            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
-            DropIndex("dbo.Users", new[] { "Profile_Id" });
-            DropIndex("dbo.RoomOccupancies", new[] { "RoomType_Id" });
-            DropIndex("dbo.Guests", new[] { "Reservation_Id" });
-            DropIndex("dbo.DailyPrices", new[] { "Reservation_Id" });
-            DropIndex("dbo.Reservations", new[] { "AspNetUsers_Id" });
-            DropIndex("dbo.Reservations", new[] { "User_Username" });
-            DropIndex("dbo.Reservations", new[] { "RoomType_Id" });
-            DropIndex("dbo.Reservations", new[] { "BillingInfo" });
-            DropIndex("dbo.Profiles", new[] { "Address_Id1" });
-            DropIndex("dbo.Profiles", new[] { "IdAspNetUsers_Id" });
-            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
-            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserRoles", new[] {"UserId"});
+            DropIndex("dbo.AspNetUserRoles", new[] {"RoleId"});
+            DropIndex("dbo.Users", new[] {"Profile_Id"});
+            DropIndex("dbo.RoomOccupancies", new[] {"RoomType_Id"});
+            DropIndex("dbo.Guests", new[] {"Reservation_Id"});
+            DropIndex("dbo.DailyPrices", new[] {"Reservation_Id"});
+            DropIndex("dbo.Reservations", new[] {"AspNetUsers_Id"});
+            DropIndex("dbo.Reservations", new[] {"User_Username"});
+            DropIndex("dbo.Reservations", new[] {"RoomType_Id"});
+            DropIndex("dbo.Reservations", new[] {"BillingInfo"});
+            DropIndex("dbo.Profiles", new[] {"Address_Id1"});
+            DropIndex("dbo.Profiles", new[] {"IdAspNetUsers_Id"});
+            DropIndex("dbo.AspNetUserLogins", new[] {"UserId"});
+            DropIndex("dbo.AspNetUserClaims", new[] {"UserId"});
             DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.Staffs");
             DropTable("dbo.Users");
--- ProjectPurple/DataAccessLayer/Migrations/201704271932098_Change-Reservation-Name.cs
+++ ProjectPurple/DataAccessLayer/Migrations/201704271932098_Change-Reservation-Name.cs
@@ -1,55 +1,54 @@
+using System.Data.Entity.Migrations;
+
 namespace DataAccessLayer.Migrations
 {
-    using System;
-    using System.Data.Entity.Migrations;
-    
     public partial class ChangeReservationName : DbMigration
     {
         public override void Up()
         {
-            DropIndex("dbo.Profiles", new[] { "Address_Id1" });
+            DropIndex("dbo.Profiles", new[] {"Address_Id1"});
             DropColumn("dbo.Profiles", "Address_Id");
-            RenameColumn(table: "dbo.Profiles", name: "IdAspNetUsers_Id", newName: "IdAspNetUsersId");
-            RenameColumn(table: "dbo.Reservations", name: "AspNetUsers_Id", newName: "AspNetUsersId");
-            RenameColumn(table: "dbo.Profiles", name: "Address_Id1", newName: "Address_Id");
-            RenameColumn(table: "dbo.DailyPrices", name: "Reservation_Id", newName: "ReservationId");
-            RenameColumn(table: "dbo.Guests", name: "Reservation_Id", newName: "ReservationId");
-            RenameColumn(table: "dbo.Reservations", name: "RoomType_Id", newName: "RoomTypeId");
-            RenameColumn(table: "dbo.Reservations", name: "User_Username", newName: "UserUsername");
-            RenameColumn(table: "dbo.RoomOccupancies", name: "RoomType_Id", newName: "RoomTypeId");
-            RenameIndex(table: "dbo.Profiles", name: "IX_IdAspNetUsers_Id", newName: "IX_IdAspNetUsersId");
-            RenameIndex(table: "dbo.Reservations", name: "IX_RoomType_Id", newName: "IX_RoomTypeId");
-            RenameIndex(table: "dbo.Reservations", name: "IX_User_Username", newName: "IX_UserUsername");
-            RenameIndex(table: "dbo.Reservations", name: "IX_AspNetUsers_Id", newName: "IX_AspNetUsersId");
-            RenameIndex(table: "dbo.DailyPrices", name: "IX_Reservation_Id", newName: "IX_ReservationId");
-            RenameIndex(table: "dbo.Guests", name: "IX_Reservation_Id", newName: "IX_ReservationId");
-            RenameIndex(table: "dbo.RoomOccupancies", name: "IX_RoomType_Id", newName: "IX_RoomTypeId");
-            AddColumn("dbo.Profiles", "AddressId", c => c.Int(nullable: false));
+            RenameColumn("dbo.Profiles", "IdAspNetUsers_Id", "IdAspNetUsersId");
+            RenameColumn("dbo.Reservations", "AspNetUsers_Id", "AspNetUsersId");
+            RenameColumn("dbo.Profiles", "Address_Id1", "Address_Id");
+            RenameColumn("dbo.DailyPrices", "Reservation_Id", "ReservationId");
+            RenameColumn("dbo.Guests", "Reservation_Id", "ReservationId");
+            RenameColumn("dbo.Reservations", "RoomType_Id", "RoomTypeId");
+            RenameColumn("dbo.Reservations", "User_Username", "UserUsername");
+            RenameColumn("dbo.RoomOccupancies", "RoomType_Id", "RoomTypeId");
+            RenameIndex("dbo.Profiles", "IX_IdAspNetUsers_Id", "IX_IdAspNetUsersId");
+            RenameIndex("dbo.Reservations", "IX_RoomType_Id", "IX_RoomTypeId");
+            RenameIndex("dbo.Reservations", "IX_User_Username", "IX_UserUsername");
+            RenameIndex("dbo.Reservations", "IX_AspNetUsers_Id", "IX_AspNetUsersId");
+            RenameIndex("dbo.DailyPrices", "IX_Reservation_Id", "IX_ReservationId");
+            RenameIndex("dbo.Guests", "IX_Reservation_Id", "IX_ReservationId");
+            RenameIndex("dbo.RoomOccupancies", "IX_RoomType_Id", "IX_RoomTypeId");
+            AddColumn("dbo.Profiles", "AddressId", c => c.Int(false));
             AlterColumn("dbo.Profiles", "Address_Id", c => c.Guid());
             CreateIndex("dbo.Profiles", "Address_Id");
         }
-        
+
         public override void Down()
         {
-            DropIndex("dbo.Profiles", new[] { "Address_Id" });
-            AlterColumn("dbo.Profiles", "Address_Id", c => c.Int(nullable: false));
+            DropIndex("dbo.Profiles", new[] {"Address_Id"});
+            AlterColumn("dbo.Profiles", "Address_Id", c => c.Int(false));
             DropColumn("dbo.Profiles", "AddressId");
-            RenameIndex(table: "dbo.RoomOccupancies", name: "IX_RoomTypeId", newName: "IX_RoomType_Id");
-            RenameIndex(table: "dbo.Guests", name: "IX_ReservationId", newName: "IX_Reservation_Id");
-            RenameIndex(table: "dbo.DailyPrices", name: "IX_ReservationId", newName: "IX_Reservation_Id");
-            RenameIndex(table: "dbo.Reservations", name: "IX_AspNetUsersId", newName: "IX_AspNetUsers_Id");
-            RenameIndex(table: "dbo.Reservations", name: "IX_UserUsername", newName: "IX_User_Username");
-            RenameIndex(table: "dbo.Reservations", name: "IX_RoomTypeId", newName: "IX_RoomType_Id");
-            RenameIndex(table: "dbo.Profiles", name: "IX_IdAspNetUsersId", newName: "IX_IdAspNetUsers_Id");
-            RenameColumn(table: "dbo.RoomOccupancies", name: "RoomTypeId", newName: "RoomType_Id");
-            RenameColumn(table: "dbo.Reservations", name: "UserUsername", newName: "User_Username");
-            RenameColumn(table: "dbo.Reservations", name: "RoomTypeId", newName: "RoomType_Id");
-            RenameColumn(table: "dbo.Guests", name: "ReservationId", newName: "Reservation_Id");
-            RenameColumn(table: "dbo.DailyPrices", name: "ReservationId", newName: "Reservation_Id");
-            RenameColumn(table: "dbo.Profiles", name: "Address_Id", newName: "Address_Id1");
-            RenameColumn(table: "dbo.Reservations", name: "AspNetUsersId", newName: "AspNetUsers_Id");
-            RenameColumn(table: "dbo.Profiles", name: "IdAspNetUsersId", newName: "IdAspNetUsers_Id");
-            AddColumn("dbo.Profiles", "Address_Id", c => c.Int(nullable: false));
+            RenameIndex("dbo.RoomOccupancies", "IX_RoomTypeId", "IX_RoomType_Id");
+            RenameIndex("dbo.Guests", "IX_ReservationId", "IX_Reservation_Id");
+            RenameIndex("dbo.DailyPrices", "IX_ReservationId", "IX_Reservation_Id");
+            RenameIndex("dbo.Reservations", "IX_AspNetUsersId", "IX_AspNetUsers_Id");
+            RenameIndex("dbo.Reservations", "IX_UserUsername", "IX_User_Username");
+            RenameIndex("dbo.Reservations", "IX_RoomTypeId", "IX_RoomType_Id");
+            RenameIndex("dbo.Profiles", "IX_IdAspNetUsersId", "IX_IdAspNetUsers_Id");
+            RenameColumn("dbo.RoomOccupancies", "RoomTypeId", "RoomType_Id");
+            RenameColumn("dbo.Reservations", "UserUsername", "User_Username");
+            RenameColumn("dbo.Reservations", "RoomTypeId", "RoomType_Id");
+            RenameColumn("dbo.Guests", "ReservationId", "Reservation_Id");
+            RenameColumn("dbo.DailyPrices", "ReservationId", "Reservation_Id");
+            RenameColumn("dbo.Profiles", "Address_Id", "Address_Id1");
+            RenameColumn("dbo.Reservations", "AspNetUsersId", "AspNetUsers_Id");
+            RenameColumn("dbo.Profiles", "IdAspNetUsersId", "IdAspNetUsers_Id");
+            AddColumn("dbo.Profiles", "Address_Id", c => c.Int(false));
             CreateIndex("dbo.Profiles", "Address_Id1");
         }
     }
