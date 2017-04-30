ff370df9887be53a87e2024ddb4c5e4f0be80151
Boyuan Yuan <by653@nyu.edu>
2017-04-30 18:01:56 -0400

    Add staff contoller

---
+++ ProjectPurple/HotelBookingWebsite/Models/StaffViewModels.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Constants;
+using System;
+
+namespace HotelBookingWebsite.Models
+{
+    public class CheckOutListModel
+    {
+        public Guid Id { get; set; }
+        public string email { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime checkInDate { get; set; }
+        public DateTime checkOutDate { get; set; }
+        public DateTime actualCheckInDate { get; set; }
+    }
+
+    public class CheckInListModel
+    {
+        public Guid Id { get; set; }
+        public string email { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime checkInDate { get; set; }
+        public DateTime checkOutDate { get; set; }
+    }
+
+    public class OccupanceyModel
+    {
+        public ROOM_TYPE type { get; set; }
+        public int inventory { get; set; }
+        public int occupancey { get; set; }
+    }
