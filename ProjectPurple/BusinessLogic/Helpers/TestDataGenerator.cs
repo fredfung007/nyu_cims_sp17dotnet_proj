7c96f54c98d434af2dbf307922c8b4128f8b2dec
Boyuan Yuan <by653@nyu.edu>
2017-05-01 18:30:25 -0400

    init commit

---
+++ ProjectPurple/BusinessLogic/Helpers/TestDataGenerator.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogic.Handlers;
+using DataAccessLayer.EF;
+using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Helpers
+{
+    public class TestDataGenerator
+    {
+        private readonly IReservationRepository _reservationRepository;
+        private readonly IRoomRepository _roomRepository;
+        private ReservationHandler _reservationHandler;
+
+        public TestDataGenerator()
+        {
+            _reservationRepository = new ReservationRepository(new CodeFirstHotelModel());
+            _roomRepository = new RoomRepository(new CodeFirstHotelModel());
+            _reservationHandler = new ReservationHandler();
+        }
+
+        public void GenerateReservations(string username, int number)
+        {
+            List<Reservation> reservations = new List<Reservation>();
+            for (int i = 0;i < number;i++)
+            {
+            }
+        }
+    }
+}
