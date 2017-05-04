b9fd4c85d8b9f1e94ed6e6460a8729c735ce2773
Boyuan Yuan <by653@nyu.edu>
2017-05-04 17:07:54 -0400

    remove staffdefault routing

--- ProjectPurple/HotelBookingWebsite/App_Start/RouteConfig.cs
+++ ProjectPurple/HotelBookingWebsite/App_Start/RouteConfig.cs
@@ -20,12 +20,6 @@ namespace HotelBookingWebsite
             );
 
             routes.MapRoute(
-                name: "StaffDefault",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Staff", action = "ViewCheckInList", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
                 name: "Additional",
                 url: "{controller}/{action}/{id}/{additional}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, additional = UrlParameter.Optional }
